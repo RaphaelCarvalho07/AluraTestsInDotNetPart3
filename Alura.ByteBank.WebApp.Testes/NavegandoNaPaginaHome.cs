ed8235aac7c1871885c63fc48b5303bc9614cb09
Raphael Carvalho <raphilskerj@gmail.com>
2022-03-31 07:02:46 -0300

    Aula 06.Criando Primeiro Teste -  02.Iniciando Com Selenium WebDriver  - Alura Tests in CSharp Part 3

---
+++ Alura.ByteBank.WebApp.Testes/NavegandoNaPaginaHome.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Alura.ByteBank.WebApp.Testes
+{
+    public class NavegandoNaPaginaHome
+    {
+        [Fact]
+        public void CarregaPaginaHomeEVerificaTituloDaPagina()
+        {
+            // Arrange
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            
+            // Act
+            driver.Navigate().GoToUrl("https://localhost:44309");
+
+            // Assert
+            Assert.Contains("WebApp", driver.Title);
+        }
+
+        [Fact]
+        public void CarregadaPaginaHomeVerificaExistenciaLinkLogEHome()
+        {
+            // Arrange
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+
+            // Act
+            driver.Navigate().GoToUrl("https://localhost:44309");
+
+            // Assert
+            Assert.Contains("Login", driver.PageSource);
+            Assert.Contains("Home", driver.PageSource);
+        }
+    }
+}
